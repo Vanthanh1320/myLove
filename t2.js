2ab0f9655c450a671a1623faeb0dc16bb6cf41f9
vthanhv <vothanh1320@gmail.com>
2024-04-21 15:11:53 +0700

    init mylove

--- t2.js
+++ t2.js
@@ -1,7 +1,7 @@
 function fadeIn(){
     var text = $('.content').text().replace(/^\s+|\s+$/g,'');
     var length = text.length;
-    console.log(text);
+
     var i = 0;
     var txt;
     var html = [];
@@ -19,15 +19,23 @@ function fadeIn(){
             },i*400);
         }(i);
     }
+
+    setTimeout(function() {
+        $('.content').append('<img src="./public/anh.jpg" class="inserted-image">');
+    }, length * 400); 
 };
 
 
 
 document.querySelector(".btn").onclick=()=>{
-    document.querySelector(".btn-wrapper").style.display="none"
-    document.querySelector("#myAudio").play()
-    document.querySelector("#heart").hidden=false
-    document.querySelector("body").style.backgroundColor="#542246"
-    document.querySelector("#heart").hidden=false
-    fadeIn()
+    if (window.innerWidth > 768) {
+        document.querySelector(".btn-wrapper").style.display="none"
+        document.querySelector("#myAudio").play()
+        document.querySelector("#heart").hidden=false
+        document.querySelector("body").style.backgroundColor="#542246"
+        document.querySelector("#heart").hidden=false
+        fadeIn()
+    } else {
+        document.querySelector(".btn-wrapper span").classList.remove("d-none")
+    }
 }
